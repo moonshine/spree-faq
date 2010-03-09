72545e83d2ae06ae99efc62f0e8be55c45f5a42c
Steph Powell <steph@endpoint.com>
2010-03-08 17:03:07 -0700

    Adding logic to show question categories that belong to user roles only.

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -2,7 +2,8 @@ class FaqsController < Spree::BaseController
   helper 'spree/base'
 
   def index
-    @categories = QuestionCategory.all :include => :questions
+    roles = current_user ? current_user.roles : [Role.find_by_name('user')]
+    @categories = roles.collect { |r| r.question_categories }.flatten.uniq
   end
 
   def show
